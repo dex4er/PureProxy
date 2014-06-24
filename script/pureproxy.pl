8f8257cdcbfabba126cf058f125d042ce57fe938
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2014-06-24 23:35:41 +0200

    POD

--- script/pureproxy.pl
+++ script/pureproxy.pl
@@ -8,6 +8,11 @@ pureproxy - a Pure Perl HTTP proxy server
 
   pureproxy --port=5000 --workers=10
 
+=head1 DESCRIPTION
+
+This is pure-Perl proxy HTTP server which can be run on almost every Perl
+installation.
+
 =cut
 
 no warnings;
@@ -55,3 +60,24 @@ my $runner = Plack::Runner->new(
 $runner->parse_options('--server-software', "PureProxy/$VERSION", @ARGV);
 
 $runner->run($app);
+
+=head1 SEE ALSO
+
+L<http://github.com/dex4er/PureProxy>.
+
+=head1 BUGS
+
+This tool has unstable features and can change in future.
+
+=head1 AUTHOR
+
+Piotr Roszatycki <dexter@cpan.org>
+
+=head1 LICENSE
+
+Copyright (c) 2014 Piotr Roszatycki <dexter@cpan.org>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
+See L<http://dev.perl.org/licenses/artistic.html>
