e003e50e4dc3b30869a3b4b9a07c6d71bf6b1d88
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-03-28 19:50:00 +0100

    Bugfix for traffic-log option and more POD documentation

--- script/pureproxy.pl
+++ script/pureproxy.pl
@@ -68,13 +68,14 @@ if ($options{traffic_log}) {
         open my $logfh, ">>", $options{traffic_log}
             or die "open($options{traffic_log}): $!";
         $logfh->autoflush(1);
-        $app = builder { enable 'TrafficLog', logger => sub { $logfh->print( @_ ) }, body_eol => $body_eol; $app; };
+        $app = builder { enable 'TrafficLog', logger => sub { $logfh->print( @_ ) }, body_eol => $body_eol, with_body => !! $body_eol; $app; };
     } else {
-        $app = builder { enable 'TrafficLog', body_eol => $body_eol; $app; };
+        $app = builder { enable 'TrafficLog', body_eol => $body_eol, with_body => !! $body_eol; $app; };
     }
 }
 
-if (not exists $options{access_log}) {
+if (not defined $runner->{access_log} or $runner->{access_log} eq '1') {
+    $runner->{access_log} = undef;
     $app = builder { enable 'AccessLog'; $app; };
 }
 
@@ -86,16 +87,60 @@ __DATA__
 
 =head1 SYNOPSIS
 
-  pureproxy --port=5000 --workers=10
+  pureproxy --host=0.0.0.0 --port=5000 --workers=10 --server Starlight
+
+  pureproxy --traffic-log=traffic.log --traffic-log-body-eol='|'
+
+  pureproxy --access-log=access.log
+
+  pureproxy --other-plackup-options
+
+  pureproxy -v
+
+  http_proxy=http://localhost:5000/ lwp-request http://www.perl.org/
+
+  https_proxy=http://localhost:5000/ lwp-request https://metacpan.org/
 
 =head1 DESCRIPTION
 
-This is pure-Perl proxy HTTP server which can be run on almost every Perl
+This is pure-Perl HTTP proxy server which can be run on almost every Perl
 installation.
+
+It supports SSL and TLS if L<IO::Socket::SSL> is installed and IPv6 if
+L<IO::Socket::IP> is installed.
+
+It can be fat-packed and then run with any system with standard Perl
+interpreter without installing other packages. See F<examples> directory
+for fat-packed version of PureProxy script.
 
 =cut
 
 __END__
+
+=head1 ENVIRONMENT
+
+=head2 PUREPROXY_SERVER
+
+Changes the default PSGI server. This is L<Thrall> for C<MSWin32> and C<cygwin>
+and L<Starlight> otherwise.
+
+=head1 INSTALLATION
+
+=head2 With cpanm(1)
+
+  $ cpanm App::PureProxy
+
+=head2 Directly
+
+  $ lwp-request http://git.io/jEE6 | sh
+
+or
+
+  $ curl -kL http://git.io/jEE6 | sh
+
+or
+
+  $ wget --quiet -O- http://git.io/jEE6 | sh
 
 =head1 SEE ALSO
 
@@ -111,7 +156,7 @@ Piotr Roszatycki <dexter@cpan.org>
 
 =head1 LICENSE
 
-Copyright (c) 2014 Piotr Roszatycki <dexter@cpan.org>.
+Copyright (c) 2014-2015 Piotr Roszatycki <dexter@cpan.org>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as perl itself.
